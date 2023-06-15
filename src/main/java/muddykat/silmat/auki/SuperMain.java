6d6d3f1edecaf923022adeb07b3e6354c9703093
Muddykat <JarrodMuddyman@gmail.com>
2023-06-15 23:55:50 +1000

    Fixed issues with jar creation
    Configuration needed

---
+++ src/main/java/muddykat/silmat/auki/SuperMain.java
@@ -0,0 +1,9 @@
+package muddykat.silmat.auki;
+
+public class SuperMain {
+    // Required for a jar to run when compiled, the main class that it uses to launch
+    // must not extend anything.
+    public static void main(String[] args){
+        ApplicationControl.main(args);
+    }
+}
